4642c235a130194caab4cd12c9bd7344fe42a403
Adel Namazi <33258157+adelorosia@users.noreply.github.com>
2024-03-12 12:37:29 +0100

    nicht finish (#7)
    Co-authored-by: Khalil <Khalil.haouas@gmail.com>

--- src/components/slider/SliderCoins.tsx
+++ src/components/slider/SliderCoins.tsx
@@ -1,13 +1,22 @@
-const SliderCoins = () => {
-    return (
-        <div>
-            <div>
-                
-            </div>
-            <div></div>
-            <div></div>
-        </div>
-    );
+export interface ISliderCoins {
+  id: string;
+  component: JSX.Element;
 }
+
+interface SliderCoinProps {
+  sliderCoin: ISliderCoins[];
+}
+
+const SliderCoins = ({ sliderCoin }: SliderCoinProps) => {
+  return (
+   <div className=" flex gap-3">
+     <div className="flex w-full" >
+      {sliderCoin.map((item, index) => (
+        <div className="flex" key={index}>{item.component}</div>
+      ))}
+    </div>
+   </div>
+  );
+};
 
 export default SliderCoins;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import MaxWithWrapper from "../components/MaxWithWrapper";
 import MainSlider, { ICarouselItem } from "../components/slider/MainSlider";
+import SliderCoins, { ISliderCoins } from "../components/slider/SliderCoins";
+import { FaBitcoin } from "react-icons/fa";
 
 const items: ICarouselItem[] = [
   {
@@ -24,10 +26,82 @@ const items: ICarouselItem[] = [
   },
 ];
 
+const sliderCoin = [
+  {
+    id:"1",
+    component:  <div className="mt-4 bg-black p-4 flex flex-col  gap-2 rounded-lg w-full">
+    <div className=" flex  items-center text-white gap-3">
+      <FaBitcoin  className=" text-xl text-yellow-400"/>
+      <p>Bitcoin</p>
+      <p>BTC/USD</p>
+    </div>
+    <div className=" flex gap-3 text-white">
+      <span className=" font-bold text-xl">USD</span><span className=" text-xl font-bold">46,168.95</span>
+    </div>
+    <div className=" flex gap-3">
+      <span className=" text-gray-200">36,641.20</span> <span className="bg-red-400 rounded-lg px-1">-0.79%</span>
+    </div>
+  </div>
+
+  },
+  {
+    id:"2",
+    component:  <div className="mt-4 bg-black  p-4 flex flex-col  gap-2 rounded-lg w-full">
+    <div className=" flex  items-center text-white gap-3">
+      <FaBitcoin  className=" text-xl text-yellow-400"/>
+      <p>Bitcoin</p>
+      <p>BTC/USD</p>
+    </div>
+    <div className=" flex gap-3 text-white">
+      <span className=" font-bold text-xl">USD</span><span className=" text-xl font-bold">46,168.95</span>
+    </div>
+    <div className=" flex gap-3">
+      <span className=" text-gray-200">36,641.20</span> <span className="bg-red-400 rounded-lg px-1">-0.79%</span>
+    </div>
+  </div>
+
+  },
+  {
+    id:"3",
+    component:  <div className="mt-4 bg-black  p-4 flex flex-col  gap-2 rounded-lg w-full">
+    <div className=" flex  items-center text-white gap-3">
+      <FaBitcoin  className=" text-xl text-yellow-400"/>
+      <p>Bitcoin</p>
+      <p>BTC/USD</p>
+    </div>
+    <div className=" flex gap-3 text-white">
+      <span className=" font-bold text-xl">USD</span><span className=" text-xl font-bold">46,168.95</span>
+    </div>
+    <div className=" flex gap-3">
+      <span className=" text-gray-200">36,641.20</span> <span className="bg-red-400 rounded-lg px-1">-0.79%</span>
+    </div>
+  </div>
+
+  },
+  {
+    id:"4",
+    component:  <div className="mt-4 bg-black  p-4 flex flex-col  gap-2 rounded-lg w-full">
+    <div className=" flex  items-center text-white gap-3">
+      <FaBitcoin  className=" text-xl text-yellow-400"/>
+      <p>Bitcoin</p>
+      <p>BTC/USD</p>
+    </div>
+    <div className=" flex gap-3 text-white">
+      <span className=" font-bold text-xl">USD</span><span className=" text-xl font-bold">46,168.95</span>
+    </div>
+    <div className=" flex gap-3">
+      <span className=" text-gray-200">36,641.20</span> <span className="bg-red-400 rounded-lg px-1">-0.79%</span>
+    </div>
+  </div>
+
+  },
+]
+
 const HomePage = () => {
   return (
     <MaxWithWrapper>
       <MainSlider items={items} />
+      <SliderCoins sliderCoin={sliderCoin}/>
     </MaxWithWrapper>
   );
 };
