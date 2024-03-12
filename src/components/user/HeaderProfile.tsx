79ab35a5aea20c9ea01ac64d5f406760de721ac8
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-03-12 16:07:26 +0100

    profile bearbeitung

--- src/components/user/HeaderProfile.tsx
+++ src/components/user/HeaderProfile.tsx
@@ -2,19 +2,25 @@ import { IoMdSettings } from "react-icons/io";
 import { useSelector } from "react-redux";
 import { RootState } from "../../feature/store";
 import { displayUser } from "../../feature/reducers/authSlice";
+import { IoIosCamera } from "react-icons/io";
 
 const HeaderProfile = () => {
   const userId = localStorage.getItem("userId");
-  
+
   const user = useSelector((state: RootState) => displayUser(state, userId!));
   return (
     <div className="my-4   ">
-      <div className="flex gap-4 font-FONT_VIGA justify-start items-center">
-        <img
-          className=" w-40 h-40 rounded-full"
-          src={user?.profile_photo}
-          alt=""
-        />
+      <div className="flex gap-4 font-FONT_VIGA justify-start items-center ">
+        <div className="relative">
+          <img
+            className=" w-40 h-40 rounded-full  "
+            src={user?.profile_photo}
+            alt=""
+          />
+          <div className="bg-SECONDARY_GRAY absolute p-1.5 rounded-full right-0 bottom-3 cursor-pointer ">
+            <IoIosCamera className="icon text-2xl " />
+          </div>
+        </div>
 
         <div className="flex flex-col gap-3 w-full">
           <div className=" flex gap-3 items-center w-full justify-between">
