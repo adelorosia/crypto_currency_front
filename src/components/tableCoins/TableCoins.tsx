211882f05d7d8abd7c1f934d0554325802b14da4
Adel Namazi <33258157+adelorosia@users.noreply.github.com>
2024-03-13 17:24:07 +0100

    nochnichtfertig (#31)
    Co-authored-by: Khalil <Khalil.haouas@gmail.com>

--- src/components/tableCoins/TableCoins.tsx
+++ src/components/tableCoins/TableCoins.tsx
@@ -1,51 +1,51 @@
 import { FaRegStar } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { displayCoins } from "../../feature/reducers/coinSlice";
+
+import { displayCoins } from "../../feature/reducers/coinSlise";
+import { RootState } from "../../feature/store";
 
 
 function TableCoins() {
+    const coins = useSelector(displayCoins);
+    const {isDarkMode} = useSelector((state:RootState)=>state.app)
 
-    const coins = useSelector(displayCoins)
-    console.log(coins)
-    
-  return (
-    <div>
-      <div className=" flex items-center justify-between">
-        <h2>Market Update</h2>
-        <button className=" bg-PRIMARY_BLUE px-4 py-1 rounded-full text-PRIMARY_WHITE">
-          View All
-        </button>
-      </div>
-      <div>
-        <table className=" w-full">
-          <thead className="capitalize text-base text-gray-100 font-medium border-b border-gray-100">
-            <tr>
-              <th className="py-2"></th>
-              <th className="py-2">#</th>
-              <th className="py-2">Last Price</th>
-              <th className="py-2">24h %</th>
-              <th className="py-2">Market Cap</th>
-              <th className="py-2">Chart</th>
-            </tr>
-          </thead>
-          <tbody>
-           {
-            coins.slice(0,10).map((coin)=>(
-                <tr className="" key={coin._id}>
-                <td><FaRegStar /></td>
-                <td>{coin?.market_cap_rank}</td>
-                <td><img src={coin?.image} alt="" /></td>
-                <td>{coin?.current_price}</td>
-                <td>{coin?.market_cap_change_percentage_24h}</td>
-                <td>{coin?.low_24h}</td>
-            </tr>
-            ))
-           }
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    return (
+        <div className="overflow-x-auto">
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-lg font-semibold">Market Update</h2>
+                <button className="bg-PRIMARY_BLUE px-4 py-1 rounded-full text-PRIMARY_WHITE">
+                    View All
+                </button>
+            </div>
+            <table className="w-full table-auto ">
+                <thead className={`text-sm border-b last:border-b-0 ${isDarkMode ? " border-PRIMARY_WHITE" : " border-PRIMARY_BLACK"}`}>
+                    <tr className="w-full text-left py-2 text-xl ">
+                      <th></th>
+                        <th className="">#</th>
+                        <th className="">Name</th>
+                        <th className=" ">Last Price</th>
+                        <th className=" ">24h %</th>
+                        <th className=" ">Market Cap</th>
+                        <th className=" ">Chart</th>
+                    </tr>
+                </thead>
+                <tbody className="text-sm ">
+                    {coins.slice(0, 10).map((coin) => (
+                        <tr className="border-b border-gray-200" key={coin?._id}>
+                            <td className="py-2"><FaRegStar /></td>
+                            <td className="py-2">{coin?.market_cap_rank}</td>
+                            <td className="py-2 flex gap-2 items-center"><img className="w-10 h-10" src={coin?.image} alt="" />{coin.name}</td>
+
+                            <td className="py-2">{coin?.current_price} $</td>
+                            <td className={`py-2 ${coin?.market_cap_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'}`}>{coin?.market_cap_change_percentage_24h}%</td>
+                            <td className="py-2">{coin?.low_24h}</td>
+                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default TableCoins;
