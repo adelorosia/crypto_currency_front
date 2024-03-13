69fb613e1a35cee3b1fb90bd08dc352f97af5c72
Ghulam Hossein Karimi <124140036+ghulamkarimi@users.noreply.github.com>
2024-03-13 11:07:50 +0100

    daten vom backend und post button erledigt

--- src/components/post/PostAdd.tsx
+++ src/components/post/PostAdd.tsx
@@ -1,41 +1,40 @@
 import { HiPhoto } from "react-icons/hi2";
 import { useSelector } from "react-redux";
 import { RootState } from "../../feature/store";
-
-const items = [
-  {
-    id: "1",
-    image: "/bild1.jpg",
-    firstName: "Hasan",
-  },
-];
+import { displayUser } from "../../feature/reducers/userSlice";
 
 const PostAdd = () => {
   const { isDarkMode } = useSelector((state: RootState) => state.app);
+  const userId = localStorage.getItem("userId");
+  const user = useSelector((state: RootState) => displayUser(state, userId!));
 
   return (
-    <div className={`w-full h-36 my-4 rounded-lg ${isDarkMode?" bg-SECONDARY_BLACK":" bg-SECONDARY_WHITE "}`}>
-      {items.map((item) => (
-        <div key={item.id}>
-          <div className="flex gap-2 px-3 py-5 ">
-            <img
-              className=" w-[46px] h-[46px] rounded-full"
-              src={item.image}
-              alt=""
-            />
-            <input
-              className={`w-full h-[46px] rounded-full px-5 font-FONT_VIGA text-lg ${isDarkMode?" bg-PRIMARY_BLACK":" bg-PRIMARY_WHITE"}`}
-              type="text"
-              placeholder={`Whats on your mind , ${item.firstName}?`}
-            />
-          </div>
-          <div className="border border-b-1  mx-3 " />
+    <div
+      className={`w-full h-36 my-4 rounded-lg ${
+        isDarkMode ? " bg-SECONDARY_BLACK" : " bg-SECONDARY_WHITE "
+      }`}
+    >
+      <div>
+        <div className="flex gap-2 px-3 py-5 ">
+          <img
+            className=" w-[46px] h-[46px] rounded-full"
+            src={user?.profile_photo}
+            alt=""
+          />
+          <input
+            className={`w-full h-[46px] rounded-full px-5 font-FONT_VIGA text-lg ${
+              isDarkMode ? " bg-PRIMARY_BLACK" : " bg-PRIMARY_WHITE"
+            }`}
+            type="text"
+            placeholder={`Whats on your mind , ${user.firstName}?`}
+          />
         </div>
-      ))}
-      <div
-        className="flex items-center justify-start mt-3 gap-2 px-5"
-      >
-        <HiPhoto
+        <div className="border border-b-1  mx-3 " />
+      </div>
+
+      <div className="flex items-center justify-between mt-3 gap-2 px-5">
+       <div className="flex items-center gap-2">
+       <HiPhoto
           className={`text-3xl ${
             isDarkMode ? "text-PRIAMRY_WHITE " : "text-PRIMARY_BLACK"
           }`}
@@ -47,6 +46,8 @@ const PostAdd = () => {
         >
           Photo
         </p>
+       </div>
+              <button className="py-1 px-14 bg-SECONDARY_GRAY rounded-xl hover:bg-PRIMARY_BLUE hover:text-SECONDARY_BLACK">post</button>
       </div>
     </div>
   );
