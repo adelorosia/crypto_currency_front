07a6971ef025be766828a73f82a81ec545a239b5
Ghulam Hossein Karimi <124140036+ghulamkarimi@users.noreply.github.com>
2024-03-13 11:49:09 +0100

    Post (#14)
    * daten vom backend und post button erledigt
    
    * fertig mit ui

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,8 +1,18 @@
 import { IoMdClose } from "react-icons/io";
-import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { BiDotsHorizontalRounded, BiSolidLike } from "react-icons/bi";
 import { BiLike } from "react-icons/bi";
 import { FaComment } from "react-icons/fa6";
 import { IoMdShareAlt } from "react-icons/io";
+
+import { FaRegComment } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { RootState } from "../../feature/store";
+import { displayUser } from "../../feature/reducers/userSlice";
+const Post = () => {
+
+  const userId= localStorage.getItem("userId")
+  const user = useSelector((state: RootState) => displayUser(state, userId!))
+ 
 import { useSelector } from "react-redux";
 import { RootState } from "../../feature/store";
 import { displayUser } from "../../feature/reducers/userSlice";
@@ -12,6 +22,7 @@ const Post = () => {
   const { isDarkMode } = useSelector((state: RootState) => state.app);
 const userId = localStorage.getItem("userId");
 const user = useSelector((state:RootState)=> displayUser(state,userId!))
+ 
 
   return (
     <div className={` rounded-xl py-2  ${isDarkMode ? "bg-SECONDARY_BLACK":"bg-SECONDARY_WHITE"}`}>
@@ -36,7 +47,7 @@ const user = useSelector((state:RootState)=> displayUser(state,userId!))
       </div>
       <div className="flex justify-between items-center px-3 mt-2">
         <div className="flex items-center gap-2">
-        <BiLike />
+        <BiSolidLike />
         <p>like</p>
         </div>
         <div className="flex gap-2">
@@ -53,14 +64,18 @@ const user = useSelector((state:RootState)=> displayUser(state,userId!))
       <div className="border border-b-1 border-gray-200  my-2 mx-3"/>
       <div>
         <div className=" flex justify-around">
-            <BiLike className="text-2xl" />
-            <FaComment className="text-2xl" />
+            <BiLike className="text-2xl hover:animate-bounce" />
+            <FaRegComment className="text-2xl hover:animate-bounce" />
         </div>
         <div className="border border-b-1 my-2 border-gray-200 mx-3" />
       </div>
       <div className="flex w-full px-3 gap-2">
         <img className="w-[40px] h-[40px] rounded-full" src={user.profile_photo} alt="" />
+ 
+        <input className="w-full pl-2 rounded-full bg-gray-200" type="text" placeholder="comment..." />
+ 
         <input className={`w-full pl-2 rounded-full ${isDarkMode ? "bg-PRIMARY_BLACK":"bg-PRIMARY_WHITE"}`} type="text" placeholder="comment..." />
+ 
       </div>
     </div>
   );
