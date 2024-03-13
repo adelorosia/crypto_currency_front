a9083b2748575f2e74d6cb656a599e69db11f4ff
Ghulam Hossein Karimi <124140036+ghulamkarimi@users.noreply.github.com>
2024-03-13 11:25:49 +0100

    user daten vom backend und theme funktion

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -3,16 +3,25 @@ import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { BiLike } from "react-icons/bi";
 import { FaComment } from "react-icons/fa6";
 import { IoMdShareAlt } from "react-icons/io";
+import { useSelector } from "react-redux";
+import { RootState } from "../../feature/store";
+import { displayUser } from "../../feature/reducers/userSlice";
+ 
+
 const Post = () => {
+  const { isDarkMode } = useSelector((state: RootState) => state.app);
+const userId = localStorage.getItem("userId");
+const user = useSelector((state:RootState)=> displayUser(state,userId!))
+
   return (
-    <div className=" bg-gray-100 rounded-xl py-2">
+    <div className={` rounded-xl py-2  ${isDarkMode ? "bg-SECONDARY_BLACK":"bg-SECONDARY_WHITE"}`}>
       <div className=" flex justify-between items-center px-2">
         <div className="flex gap-2">
         <div>
-          <img className="w-[50px] h-[50px] rounded-full" src="/bild1.jpg" alt="" />
+          <img className="w-[50px] h-[50px] rounded-full" src={user.profile_photo} alt="" />
         </div>
         <div className="flex flex-col">
-          <p>Hasan</p>
+          <p>{user.firstName}</p>
           <p className="text-slate-400 text-xs">March 8 at 5:00 AM</p>
         </div>
         </div>
@@ -21,7 +30,7 @@ const Post = () => {
           <IoMdClose />
         </div>
       </div>
-      <p className="my-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum praesentium saepe delectus magni voluptatem laudantium. Voluptas, culpa eveniet? Nesciunt, voluptatum iusto a sapiente quae voluptatibus ut, ipsam eum illum distinctio voluptatem non atque porro harum? Error aspernatur aliquid voluptatum repellendus.</p>
+      <p className="my-2 px-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum praesentium saepe delectus magni voluptatem laudantium. Voluptas, culpa eveniet? Nesciunt, voluptatum iusto a sapiente quae voluptatibus ut, ipsam eum illum distinctio voluptatem non atque porro harum? Error aspernatur aliquid voluptatum repellendus.</p>
       <div>
         <img className="w-full h-[70vh]" src="/bild2.jpg" alt="" />
       </div>
@@ -50,8 +59,8 @@ const Post = () => {
         <div className="border border-b-1 my-2 border-gray-200 mx-3" />
       </div>
       <div className="flex w-full px-3 gap-2">
-        <img className="w-[40px] h-[40px] rounded-full" src="/bild1.jpg" alt="" />
-        <input className="w-full pl-2 rounded-full bg-gray-200" type="text" placeholder="comment..." />
+        <img className="w-[40px] h-[40px] rounded-full" src={user.profile_photo} alt="" />
+        <input className={`w-full pl-2 rounded-full ${isDarkMode ? "bg-PRIMARY_BLACK":"bg-PRIMARY_WHITE"}`} type="text" placeholder="comment..." />
       </div>
     </div>
   );
